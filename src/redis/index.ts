7c9b64b3e155d9d32633da06a485caf890632360
Bernardo Garciarivas <brgarciarivas@gmail.com>
2022-11-23 13:41:43 -0600

    update env var for prod

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from "redis";
 
 const redis = createClient({
-  url: "rediss://:p6d65d5475e414c8d356de3af5d32afc7767b37ac3c0107704e9966fa5bdb3499@ec2-54-146-251-58.compute-1.amazonaws.com:24989",
+  url: process.env.REIDS_URL,
   socket: {
     tls: true,
     rejectUnauthorized: false,
