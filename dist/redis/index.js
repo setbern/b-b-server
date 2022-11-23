80cbbda41c75535c202c98eddb77daf59d28ddd7
Bernardo Garciarivas <brgarciarivas@gmail.com>
2022-11-23 13:42:03 -0600

    add new things

--- dist/redis/index.js
+++ dist/redis/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const redis_1 = require("redis");
 const redis = (0, redis_1.createClient)({
-    url: "rediss://:p6d65d5475e414c8d356de3af5d32afc7767b37ac3c0107704e9966fa5bdb3499@ec2-54-146-251-58.compute-1.amazonaws.com:24989",
+    url: process.env.REIDS_URL,
     socket: {
         tls: true,
         rejectUnauthorized: false,
