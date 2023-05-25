73acd469fd1979a52cbe8697662e6866b030bde7
Ricardo Menotti <ricardomenotti31@gmail.com>
2023-05-24 19:03:33 -0500

    almost done

--- src/nft/services/addAmount.service.ts
+++ src/nft/services/addAmount.service.ts
@@ -14,33 +14,63 @@ const addAmount = async () => {
   // get all the collections
   const collections = await redis.hGetAll("3:COLLECTION");
   // get the new balance of token in the collection
+  console.log("collections", collections);
+  let index = 0;
+  const nftsToCheckBalance: any = [];
   Object.keys(collections).forEach(async (key) => {
     const parsedCollection = JSON.parse(collections[key]);
+    // console.log('parsedCollection', parsedCollection)
     const [collectionAddress, collectionName] = key.split(".");
     // check if collection is a badger
 
     const promise = await Promise.all(
       Object.keys(parsedCollection).map((token) => {
-        const nftsToCheckBalance: any = [];
-        let index = 0;
-        if (nftsToCheckBalance[index] === undefined) {
-          nftsToCheckBalance.push([]);
+        if (nftsToCheckBalance.length === 0) {
+          console.log(
+            "undefined",
+            nftsToCheckBalance[index],
+            index,
+            nftsToCheckBalance
+          );
+
+          return nftsToCheckBalance.push([
+            {
+              stxVal: someCV(uintCV(token)),
+              jsVal: token,
+              collection: key,
+            },
+          ]);
         }
+        console.log("outside", index, nftsToCheckBalance);
         if (nftsToCheckBalance[index].length > 4) {
+          console.log("inside", index);
           index = index + 1;
+          console.log("index", index);
+          nftsToCheckBalance[index] = [
+            {
+              stxVal: someCV(uintCV(token)),
+              jsVal: token,
+              collection: key,
+            },
+          ];
+          return;
         }
         nftsToCheckBalance[index].push({
           stxVal: someCV(uintCV(token)),
           jsVal: token,
-          collection:key
+          collection: key,
         });
+        // console.log('nftsToCheckBalance', nftsToCheckBalance, token, index)
         return nftsToCheckBalance;
       })
-    ).then((nftsToCheckBalance) => {
-      return nftsToCheckBalance[0];
+    ).then(() => {
+     
+      return nftsToCheckBalance;
     });
+    // console.log('promise', promise)
 
     promise.forEach(async (list: any) => {
+      console.log("list", list);
       const readOnlyCallBoardIndex = await callReadOnlyFunction({
         contractName: CONTRACT_NAME,
         contractAddress: CONTRACT_ADDRESSS,
@@ -53,6 +83,7 @@ const addAmount = async () => {
         network: new StacksMainnet(),
       });
       const cleanValue = cvToJSON(readOnlyCallBoardIndex).value.value;
+      // console.log('cleanValue', cleanValue)
 
       cleanValue.forEach(async (value: any, index: number) => {
         const tileAmount = parseInt(value.value);
@@ -61,8 +92,8 @@ const addAmount = async () => {
         const rawCollection = await redis.hGet("3:COLLECTION", collectionId);
         const collection = JSON.parse(rawCollection as string);
 
-        const data = {...collection, [tokenId]: tileAmount };
-        console.log(data)
+        const data = { ...collection, [tokenId]: tileAmount };
+        console.log(data);
         await redis.hSet("3:COLLECTION", collectionId, JSON.stringify(data));
       });
     });
